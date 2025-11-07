2d42a65898f7f2af11df19c688cf96f4c9fab4b0
liqiang <wbliqiang@centaline.com.cn>
2025-11-07 15:45:20 +0800

    SpringBoot 项目构建

---
+++ spring-boot-auto-assembly/src/main/java/com/lq/springbootautoassembly/SpringBootAutoAssemblyApplication.java
@@ -0,0 +1,18 @@
+package com.lq.springbootautoassembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @ClassName SpringBootAutoAssemblyApplication
+ * @Description
+ * @author liqiang
+ * @Date 2025/11/5 17:40
+ */
+@SpringBootApplication
+public class SpringBootAutoAssemblyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoAssemblyApplication.class, args);
+    }
+
+}
---
+++ spring-boot-auto-assembly/src/test/java/com/lq/springbootautoassembly/SpringBootAutoAssemblyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lq.springbootautoassembly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoAssemblyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
